0f8969e51230731a42c45e378e65acb2b795dee9
Valentin Bontemps <valentin@chaplean.com>
2015-12-08 13:43:07 +0100

    add publication status fixtures

---
+++ DataFixtures/ORM/LoadPublicationStatusData.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Chaplean\Bundle\CmsBundle\DataFixtures\ORM;
+
+use Chaplean\Bundle\CmsBundle\Entity\PublicationStatus;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+/**
+ * LoadPublicationStatusData.php.
+ *
+ * @author    Valentin - Chaplean <valentin@chaplean.com>
+ * @copyright 2014 - 2015 Chaplean (http://www.chaplean.com)
+ * @since     1.0.0
+ */
+class LoadPublicationStatusData extends AbstractFixture
+{
+    /**
+     * @param ObjectManager $manager
+     *
+     * @return void
+     */
+    public function load(ObjectManager $manager)
+    {
+        $status = new PublicationStatus();
+        $status->setPosition(1);
+        $status->setKeyname('published');
+        $manager->persist($status);
+
+        $status = new PublicationStatus();
+        $status->setPosition(2);
+        $status->setKeyname('unpublished');
+        $manager->persist($status);
+
+        $status = new PublicationStatus();
+        $status->setPosition(3);
+        $status->setKeyname('archived');
+        $manager->persist($status);
+
+        $manager->flush();
+    }
+}
