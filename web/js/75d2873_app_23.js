a6efffe86cc7b72bd77be7201f6aca4b8ec20902
Valentin Bontemps <valentin@chaplean.com>
2016-05-16 16:52:10 +0200

    udpate js

---
+++ web/js/75d2873_app_23.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var cms = angular.module('Cms', [
+    'ngAnimate', 'ngResource', 'ngSanitize', 'ngBootbox', 'ui.bootstrap',
+    'ui.mask', 'boxuk.translation', 'textAngular', 'smart-table',
+    'angularUtils.directives.dirPagination', 'angularMoment', 'angularFileUpload'
+
+]);
+
+cms.config(function ($interpolateProvider) {
+    $interpolateProvider.startSymbol('{^').endSymbol('^}');
+});
+
+cms.run(function(amMoment) {
+    amMoment.changeLocale(locale);
+});
+
+cms.controller('MainController', function($scope, $rootScope, Post, CmsAlertService, $ngBootbox, TranslationService, CmsRouter) {
+
+    $ngBootbox.addLocale('fr', {
+        OK:      TranslationService.trans('button.validate.global'),
+        CANCEL:  TranslationService.trans('button.cancel.global'),
+        CONFIRM: TranslationService.trans('button.confirm.global')
+    });
+    $ngBootbox.setLocale(locale);
+
+    $rootScope.path = function (url, options) {
+        return Routing.generate(url, options);
+    };
+
+    $scope.alerts = CmsAlertService.alerts;
+
+    $scope.closeAlert = function (index) {
+        CmsAlertService.closeAlert(index);
+    };
+
+    $scope.CmsRouter = CmsRouter;
+    $scope.menu = {
+        active: ''
+    };
+
+    $scope.activeMenu = function (menu) {
+        $scope.menu.active = menu;
+    }
+});
