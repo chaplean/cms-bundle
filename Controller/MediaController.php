f69d60bbe272c31bb559d9b9367b88a21e16832d
hugo dumazeau <hugo@chaplean.com>
2017-05-24 16:06:55 +0200

    #20200, change $path in downloadAction

--- Controller/MediaController.php
+++ Controller/MediaController.php
@@ -4,6 +4,7 @@ namespace Chaplean\Bundle\CmsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -17,23 +18,30 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 class MediaController extends Controller
 {
     /**
-     * @param string $path
+     * @param Request $request
      *
      * @return Response
      */
-    public function downloadAction($path)
+    public function downloadAction(Request $request)
     {
+        $path = $request->get('path');
+        $name = $request->get('fileName');
         $fs = new Filesystem();
         $path = $this->getParameter('kernel.root_dir') . '/../web/' . $path;
 
         if (!$fs->exists($path)) {
             throw new NotFoundHttpException;
         }
+        if ($name) {
+            $fileName = $name;
+        } else {
+            $fileName = explode($path, '/');
+            $fileName = array_pop($fileName);
+        }
 
-        $fileName = explode($path, '/');
         return new Response(file_get_contents($path), 200, array(
             'Content-Type' => 'application/force-download',
-            'Content-Disposition' => 'attachment; filename="' . array_pop($fileName) . '"'
+            'Content-Disposition' => 'attachment; filename="' . $fileName . '"'
         ));
     }
 }
